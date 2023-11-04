293282b826ad68bcec0d4385669cf0ffb2090b75
JokingPhantom <JokingPhantom@users.noreply.github.com>
2023-11-03 19:16:03 -0700

    updated host service

--- info.py
+++ info.py
@@ -12,5 +12,5 @@ class Info(commands.Cog):
     @commands.command(name='about_me', help='Returns information about the bot.')
     async def about_me(self, ctx):
         await ctx.send('Author: JokingPhantom/Valcrist77')
-        await ctx.send('Current host location: Amazon EC2, US East')
+        await ctx.send('Current host location: Amazon Lightsail, US East')
         await ctx.send('https://github.com/JokingPhantom/swat-discord-bot')
