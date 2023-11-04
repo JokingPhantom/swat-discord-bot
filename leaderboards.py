6148a85d2d15262c20c1afd0909f6b45f7fcd44a
JokingPhantom <JokingPhantom@users.noreply.github.com>
2023-11-03 19:16:03 -0700

    update leaderboards module to handle files larger than 2000, and arbitrary rank start number

--- leaderboards.py
+++ leaderboards.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python
 from discord.ext import commands
+from itertools import islice, zip_longest
 
 class Leaderboards(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='leaderboard', aliases=['leaderboards'], help='Shows records of fastest Extinctions, Legendary Extinctions, and Nightmares. Valid names are: exts, legendary_exts, nms, solo_nms. Ping Valcrist to update this list.')
-    async def leaderboard(self, ctx, name):
-        f = open(''.join(['fastest_', name, '.txt']), 'r')
-        file_contents = f.read()
-        await ctx.send(file_contents)
+    @commands.command(name='leaderboard', aliases=['leaderboards'], help='Shows records of fastest Extinctions, Legendary Extinctions, and Nightmares. Valid names are: exts, legendary_exts, nms, solo_nms. Will limit ranks to top 10 or 2000 characters, whichever is shorter. Use start number to indicate which rank start from, defaults to 1. Post replay file and screenshot of "Official Time" in #replays or DM to Valcrist to update this list.')
+    async def leaderboard(self, ctx, name, start = 1):
+        filename = ''.join(['fastest_', name, '.txt'])
+        result = ''
+
+        fp = open(filename)
+        for i, line in enumerate(fp):
+            if start <= i:
+                entry = f'{i}. {line}'
+                if len(result) + len(entry) < 2000:
+                    result += entry
+                else:
+                    break
+        await ctx.send(result)
