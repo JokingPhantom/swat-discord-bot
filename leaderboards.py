7c62409a6cdd2687083f8bbf64aa2d26f2b8f125
JokingPhantom <JokingPhantom@users.noreply.github.com>
2023-11-03 19:16:03 -0700

    leaderboards for solo nm

--- leaderboards.py
+++ leaderboards.py
@@ -5,7 +5,7 @@ class Leaderboards(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='leaderboard', help='Shows records of fastest Extinctions, Legendary Extinctions, and Nightmares. Valid names are: exts, legendary_exts, nms. Ping Valcrist to update this list.')
+    @commands.command(name='leaderboard', help='Shows records of fastest Extinctions, Legendary Extinctions, and Nightmares. Valid names are: exts, legendary_exts, nms, solo_nms. Ping Valcrist to update this list.')
     async def leaderboard(self, ctx, name):
         f = open(''.join(['fastest_', name, '.txt']), 'r')
         file_contents = f.read()
